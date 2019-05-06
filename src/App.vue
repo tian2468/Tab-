9da51a5a367345f019f6a79969e9ff722eda884b
tzz2468 <15075318235@163.com>
2019-05-06 17:59:37 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,93 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul class="swiperTab">
+      <li
+        @click="tabClick(index)"
+        v-for="(item,index) in navList"
+        :key="index"
+        :class="index==initialSlide?'active':''"
+      >
+        <span>{{item}}</span>
+      </li>
+    </ul>
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" @change="swiperChange()">内容一</div>
+        <div class="swiper-slide">内容二</div>
+        <div class="swiper-slide">内容三</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Swiper from "swiper";
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {
+      navList: ["内容一", "内容二", "内容三"],
+      initialSlide: 0,
+      mySwiper: ""
+    };
+  },
+  mounted() {
+    let that = this;
+    that.dom();
+    that.mySwiper = new Swiper(".swiper-container", {//初始化swiper
+      initialSlide: this.initialSlide,//swiper开始内容页详情看swiperAPI
+      observer: true
+    });
+    that.mySwiper.on("slideChange", function() {//监听swiper-slide切换
+      that.initialSlide = this.realIndex;
+    });
+  },
+  methods: {
+    dom() {//fuzh
+      let W = window.innerWidth;
+      let H = window.innerHeight;
+      let doms = document.getElementById("app");
+      doms.style.width = W + "px";
+      doms.style.height = H + "px";
+    },
+    tabClick(index) {//点击导航切换
+      this.initialSlide = index;
+      this.mySwiper.slideTo(index);//跳转到指定内容
+    }
+  }
+};
+</script>
+<style lang="scss">
+@import "../public/css/global.scss";
+.active {
+  color: red !important;
+}
+.swiperTab {
+  width: 100%;
+  height: 10%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  li {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: px2rem(20);
+    color: #000;
   }
 }
-</script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.swiper-container {
+  width: 100%;
+  height: 90%;
+  .swiper-wrapper {
+    font-size: px2rem(30);
+    .swiper-slide {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Swiper from 'swiper'
+import '../public/css/global.scss'
+import '../node_modules/swiper/dist/css/swiper.min.css'
 
 Vue.config.productionTip = false
 
